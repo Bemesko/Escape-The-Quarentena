17f03110b06dc536b1f731f269ebc7d4525572f5
Bernardo Mesko <bemesko@gmail.com>
2020-09-15 21:04:41 -0300

    Implemented Web Icon

---
+++ Escape The Room/Assets/Scripts/StoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    public static StoryManager Instance { get; private set; }
+    public bool rememberedCoat = false;
+    public bool brokenKey = false;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
